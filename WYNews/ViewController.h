603d1dedf883337a5436ee277858779526b1e029
yanyin <yanyin1986@gmail.com>
2015-08-27 14:50:11 +0800

    初始化创建一个工程

---
+++ WYNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYNews
+//
+//  Created by yanyin on 15/8/27.
+//  Copyright (c) 2015年 yanyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
